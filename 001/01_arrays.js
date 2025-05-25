e456d862271e0d9c64e5c371c489629647d48c6f
utkarsh <133217805+ITACHI161105@users.noreply.github.com>
2025-05-25 15:39:10 +0000

    saved 01

--- 001/01_arrays.js
+++ 001/01_arrays.js
@@ -21,15 +21,15 @@
 
 
 
-const obj1 = {1:"a", 2:"b"}
-const obj2 = {3:"c", 4:"d"} 
-const obj4 = {5:"u", 6:"s"}
+//const obj1 = {1:"a", 2:"b"}
+//const obj2 = {3:"c", 4:"d"} 
+//const obj4 = {5:"u", 6:"s"}
 
 // const obj3 = { obj1 , obj2 }
 
-const obj3 = Object.assign({},obj1,obj2,obj4);
+//const obj3 = Object.assign({},obj1,obj2,obj4);
 
 
 
 
-console.log(obj3);
+//console.log(obj3);
